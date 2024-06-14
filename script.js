f02b9a64f22494722c86ee8124d5d076b3d36b37
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-06-13 20:01:51 -0700

    query selected the continue button and quiz section class

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
 
 startBtn.onclick = () => { 
     popupInfo.classList.add('active');
