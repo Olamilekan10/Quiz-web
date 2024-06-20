e880432e655aac9d90dc7b41575c113116624bb0
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-06-19 21:46:48 -0700

    wrote a function to increase user score in every correctly picked answer

--- script.js
+++ script.js
@@ -24,10 +24,12 @@ continueBtn.onclick = () => {
 
     showQuestions(0);
     questionCounter(1); 
+    headerScore();
 };
 
 let questionCount = 0;
 let questionNumb = 1;
+let userScore = 0;
 
 const nextBtn = document.querySelector('.next-btn')
 
@@ -71,6 +73,8 @@ function optionSelected(answer) {
 
     if (userAnswer == correctAnswer) {
         answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
     } 
     else {
         answer.classList.add('incorrect');
@@ -92,4 +96,9 @@ function optionSelected(answer) {
 function questionCounter(index) {
     const questionTotal = document.querySelector('.question-total');
     questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore () {
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `${userScore} / ${questions.length}`;
 }
