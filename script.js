2d1c690f64c3060bec03620a1557c316aeb26b16
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-06-23 23:07:03 -0700

    query selected two constant variable tryagain and gohome button

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
 
 startBtn.onclick = () => { 
     popupInfo.classList.add('active');
